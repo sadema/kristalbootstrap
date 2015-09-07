cea806b3124bb3b9b79dfd5b7fe3afe74f145ad8
sjoerdadema <sjoerd@kristalsoftware.nl>
2015-09-07 20:01:01 +0200

    initial commit

---
+++ kristalcms/src/main/java/nl/kristalsoftware/jaxrstest/data/Customer.java
@@ -0,0 +1,29 @@
+package nl.kristalsoftware.jaxrstest.data;
+
+import org.jboss.resteasy.links.RESTServiceDiscovery;
+
+import javax.xml.bind.annotation.*;
+
+/**
+ * Created by sjoerdadema on 07-09-15.
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement
+public class Customer {
+
+    @XmlID
+    @XmlAttribute(name="id")
+    private String nodename;
+
+    @XmlElement
+    private String city = "Nunspeet";
+
+    @XmlElementRef
+    private RESTServiceDiscovery rest;
+
+    public Customer() {}
+
+    public Customer(String customerId) {
+        nodename = customerId;
+    }
+}
---
+++ kristalcms/src/main/java/nl/kristalsoftware/jaxrstest/resource/CustomerResourceImpl.java
@@ -0,0 +1,34 @@
+package nl.kristalsoftware.jaxrstest.resource;
+
+import nl.kristalsoftware.jaxrstest.data.Customer;
+import org.jboss.resteasy.links.AddLinks;
+import org.jboss.resteasy.links.LinkResource;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import java.util.logging.Logger;
+
+/**
+ * Created by sjoerdadema on 07-09-15.
+ */
+@Path("/customers")
+@Produces("application/xml")
+public class CustomerResourceImpl {
+
+    @Inject
+    private Logger logger;
+
+    @AddLinks
+    @LinkResource
+    @GET
+    @Path("{customerId}")
+    public Customer getCustomer(@PathParam("customerId") String customerId, @Context UriInfo uriInfo) {
+        logger.info(uriInfo.getPath());
+        return new Customer(customerId);
+    }
+}
